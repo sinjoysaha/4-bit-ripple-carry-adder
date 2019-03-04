53172fe9de47964e581b7ada2701c31df4caf380
Sinjoy Saha <42317106+sinjoysaha@users.noreply.github.com>
2019-03-04 18:53:13 +0530

    Update bool_test.v

--- behavioral-rep/bool_test.v
+++ behavioral-rep/bool_test.v
@@ -4,12 +4,12 @@ module testbench;
   bool_full_adder DUT(S, Cy, A, B, C);
   initial begin
     $dumpfile("bool_full_adder.vcd");
-	$dumpvars(0, testbench);
-	$monitor($time, " A=%b,  B=%b, C=%b, S=%b, Cy=%b", A, B, C, S, Cy);
-	#5 A=1; B=0; C=0;
-	#5 A=1; B=1; C=0;
-	#5 A=1; B=1; C=1;
-	#5 A=0; B=1; C=0;
-	#5 $finish;
+    $dumpvars(0, testbench);
+    $monitor($time, " A=%b,  B=%b, C=%b, S=%b, Cy=%b", A, B, C, S, Cy);
+    #5 A=1; B=0; C=0;
+    #5 A=1; B=1; C=0;
+    #5 A=1; B=1; C=1;
+    #5 A=0; B=1; C=0;
+    #5 $finish;
   end
 endmodule
