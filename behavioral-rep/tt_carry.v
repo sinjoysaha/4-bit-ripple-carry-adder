55afa52d7443af196d7a51621eaaa89fc5ef9de1
Sinjoy Saha <42317106+sinjoysaha@users.noreply.github.com>
2019-03-04 18:52:43 +0530

    Update tt_carry.v

--- behavioral-rep/tt_carry.v
+++ behavioral-rep/tt_carry.v
@@ -4,10 +4,10 @@ primitive carry(Cy, A, B, C);
   table
   //A B C   Cy
     1 1 ?  : 1;
-	1 ? 1  : 1;
-	? 1 1  : 1;
-	0 0 ?  : 0;
-	0 ? 0  : 0;
-	? 0 0  : 0;
+    1 ? 1  : 1;
+    ? 1 1  : 1;
+    0 0 ?  : 0;
+    0 ? 0  : 0;
+    ? 0 0  : 0;
   endtable
 endprimitive
