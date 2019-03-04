e8582d4265851e877e034e51b707d4d72409aa49
Sinjoy Saha <42317106+sinjoysaha@users.noreply.github.com>
2019-03-04 18:51:57 +0530

    Update tt_test.v

--- behavioral-rep/tt_test.v
+++ behavioral-rep/tt_test.v
@@ -4,12 +4,12 @@ module testbench;
   carry DUT(Cy, A, B, C);
   initial begin
     $dumpfile("tt_carry.vcd");
-	$dumpvars(0, testbench);
-	$monitor($time, " A=%b,  B=%b, C=%b, Cy=%b", A, B, C, Cy);
-	#5 A=1; B=0; C=0;
-	#5 A=1; B=1; C=0;
-	#5 A=1; B=1; C=1;
-	#5 A=0; B=1; C=0;
-	#5 $finish;
+    $dumpvars(0, testbench);
+    $monitor($time, " A=%b,  B=%b, C=%b, Cy=%b", A, B, C, Cy);
+    #5 A=1; B=0; C=0;
+    #5 A=1; B=1; C=0;
+    #5 A=1; B=1; C=1;
+    #5 A=0; B=1; C=0;
+    #5 $finish;
   end
 endmodule
