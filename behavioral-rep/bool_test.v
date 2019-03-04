64a093aa87a8f9e6ad069cf15b48be295dcdf85b
Sinjoy Saha <sinjoysaha555@gmail.com>
2019-03-04 18:49:05 +0530

    tabs to spaces

--- behavioral-rep/bool_test.v
+++ behavioral-rep/bool_test.v
@@ -1,16 +1,15 @@
 module testbench;
-	reg A, B, C;
-	wire S, Cy;
-	bool_full_adder DUT(S, Cy, A, B, C);
-	initial
-		begin
-			$dumpfile("bool_full_adder.vcd");
-			$dumpvars(0, testbench);
-			$monitor($time, " A=%b,  B=%b, C=%b, S=%b, Cy=%b", A, B, C, S, Cy);
-			#5 A=1; B=0; C=0;
-			#5 A=1; B=1; C=0;
-			#5 A=1; B=1; C=1;
-			#5 A=0; B=1; C=0;
-			#5 $finish;
-		end
+  reg A, B, C;
+  wire S, Cy;
+  bool_full_adder DUT(S, Cy, A, B, C);
+  initial begin
+    $dumpfile("bool_full_adder.vcd");
+	$dumpvars(0, testbench);
+	$monitor($time, " A=%b,  B=%b, C=%b, S=%b, Cy=%b", A, B, C, S, Cy);
+	#5 A=1; B=0; C=0;
+	#5 A=1; B=1; C=0;
+	#5 A=1; B=1; C=1;
+	#5 A=0; B=1; C=0;
+	#5 $finish;
+  end
 endmodule
--- behavioral-rep/tt_carry.v
+++ behavioral-rep/tt_carry.v
@@ -1,13 +1,13 @@
 primitive carry(Cy, A, B, C);
-	input A, B, C;
-	output Cy;
-	table
-	  //A B C   Cy
-		1 1 ?  : 1;
-		1 ? 1  : 1;
-		? 1 1  : 1;
-		0 0 ?  : 0;
-		0 ? 0  : 0;
-		? 0 0  : 0;
-	endtable
+  input A, B, C;
+  output Cy;
+  table
+  //A B C   Cy
+    1 1 ?  : 1;
+	1 ? 1  : 1;
+	? 1 1  : 1;
+	0 0 ?  : 0;
+	0 ? 0  : 0;
+	? 0 0  : 0;
+  endtable
 endprimitive
